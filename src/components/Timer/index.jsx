a6fb0b2c6fb3eb78530a7a641ad907d9c13e3751
aurore-ry <aremy.dev@gmail.com>
2021-11-18 14:07:56 +0100

    Refactor timer

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -1,51 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import "./styles.css";
 
+const TIMER_SECONDS_DEFAULT = 25 * 60;
+const PAUSE_TIME_SECONDS_DEFAULT = 5 * 60;
+const ONE_SECOND_MS = 1 * 1000;
+
 export const Timer = ({ title }) => {
-  const runtime = 0;
+  const [timerSeconds, setTimerSeconds] = useState(TIMER_SECONDS_DEFAULT);
+  const [pauseTimeSeconds, setPauseTimeSeconds] = useState(
+    PAUSE_TIME_SECONDS_DEFAULT
+  );
+  const [intervalId, setIntervalId] = useState(null);
 
-  const [seconds, setSeconds] = useState(25 * 60);
-  const [paused, setPaused] = useState(true);
+  const onTimerUpdate = useCallback(() => {
+    setTimerSeconds((prevSeconds) => prevSeconds - 1);
+  }, [setTimerSeconds]);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      console.log(`${Date.now()} - paused: ${paused}`);
+  const play = useCallback(() => {
+    if (intervalId != null) {
+      return;
+    }
 
-      if (seconds === runtime) {
-        return paused;
-      } else if (!paused) {
-        setSeconds((sec) => sec - 1);
-      }
-    }, 1000);
-    return () => {
-      clearInterval(interval);
-    };
-  }, [paused, seconds]);
+    setIntervalId(setInterval(onTimerUpdate, ONE_SECOND_MS));
+  }, [intervalId, setIntervalId, onTimerUpdate]);
 
-  function play() {
-    setPaused(false);
-  }
-  function stop() {
-    setPaused(true);
-    // setSeconds(0);
-  }
+  const pause = useCallback(() => {
+    clearInterval(intervalId);
+    setIntervalId(null);
+  }, [intervalId, setIntervalId]);
 
-  function RefreshTimer() {
-    setPaused(true);
-    setSeconds(25 * 60);
-  }
-
+  const stop = useCallback(() => {
+    clearInterval(intervalId);
+    setIntervalId(null);
+    setTimerSeconds(TIMER_SECONDS_DEFAULT);
+  }, []);
   return (
     <>
       <h3>{(title = "Time to focus!")}</h3>
       <p id="tiktak">
-        {`${Math.floor(seconds / 60)}:${("00" + (seconds % 60)).slice(-2)}`}
+        {`${Math.floor(timerSeconds / 60)}:${("00" + (timerSeconds % 60)).slice(
+          -2
+        )}`}
       </p>
-      <button onClick={paused ? play : stop} className="button">
-        {paused ? "Start" : "Pause"}
+      <button onClick={!intervalId ? play : pause} className="button">
+        {!intervalId ? "Start" : "Pause"}
       </button>
-      <button onClick={RefreshTimer} className="button">
+      <button onClick={stop} className="button">
         Reset
       </button>
     </>
