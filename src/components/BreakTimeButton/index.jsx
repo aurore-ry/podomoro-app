2144acc29efaae1b3ddb0c0207049b4dbc1749d6
aurore-ry <aremy.dev@gmail.com>
2021-11-19 12:11:32 +0100

    break time logic

--- src/components/BreakTimeButton/index.jsx
+++ src/components/BreakTimeButton/index.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 import { formatSecondsAsMinutesSeconds } from "../../utils/formatSecondsAsMinutesSeconds";
 
+import "./styles.css";
+
 export const BreakTimeButton = ({ seconds, onClick }) => {
   return (
-    <button onClick={onClick}>{formatSecondsAsMinutesSeconds(seconds)}</button>
+    <button onClick={onClick} className="break-time-buttons">
+      {formatSecondsAsMinutesSeconds(seconds)}
+    </button>
   );
 };
